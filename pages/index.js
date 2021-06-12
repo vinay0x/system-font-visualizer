39567e59fbd3850ada074f1bbb67fa75ae512026
Vinay Chandran <vinaychandranvs@gmail.com>
2021-06-12 23:58:54 +0530

    Init

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,61 @@
-import Head from 'next/head'
+import Head from "next/head";
+
+String.prototype.capitalize = function () {
+	return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+const cartesianProduct = (...a) =>
+	a.reduce((a, b) => a.flatMap((d) => b.map((e) => [d, e].flat())));
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	const fontSizes = [
+		{ value: "xs", name: "12px" },
+		{ value: "sm", name: "14px" },
+		{ value: "base", name: "16px" },
+		{ value: "lg", name: "18px" },
+		{ value: "xl", name: "20px" },
+		{ value: "2xl", name: "24px" },
+	];
+	const fontWeights = [
+		{ value: "light", name: "300" },
+		{ value: "regular", name: "400" },
+		{ value: "medium", name: "500" },
+		{ value: "semibold", name: "600" },
+		{ value: "bold", name: "700" },
+		{ value: "extrabold", name: "800" },
+	];
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+	const fontParams = cartesianProduct(fontSizes, fontWeights);
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen py-2">
+			<Head>
+				<title>Create Next App</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main className="p-12">
+        <div className="flex flex-col pb-4 space-y-2 border-b border-gray-50">
+				<h1 className="text-3xl font-semibold">
+					System font rendering visualizer
+				</h1>
+				<code className="p-4 text-xs rounded bg-gray-50">
+					body {"{"} font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+					               Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif; {"}"}
+				</code>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+				{fontParams.map(([fontSize, fontWeight]) => (
+					<div className="grid grid-cols-8 p-6 pl-0 border-b border-gray-50">
+						<div className="col-span-2 space-y-2 text-xs text-gray-500 rounded">
+							{fontSize.name.capitalize()}, {fontWeight.name} weight
+						</div>
+						<div
+							className={`text-${fontSize.value} font-${fontWeight.value} col-span-6`}
+						>
+							The world looks mighty good to me, cause Tootsie Rolls are all I
+							see.
+						</div>
+					</div>
+				))}
+			</main>
+		</div>
+	);
 }
